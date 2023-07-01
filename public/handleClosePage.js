c69aef768fba1e89e70464b3641f2063cf317a3c
Nahuel <nahuel.lorenzodev@gmail.com>
2023-07-01 16:16:40 -0300

    change name of function handlerClosePage

--- public/handleClosePage.js
+++ public/handleClosePage.js
@@ -1,7 +1,7 @@
-function handlerClosePage() {
+function handleClosePage() {
   const $closePage = document.querySelector('.cross-close');
   $closePage.addEventListener('click', () => {
     location.replace('/');
   });
 }
-handlerClosePage();
+handleClosePage();
