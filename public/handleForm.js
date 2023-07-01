60d61b7b28ebeb3e6814145f4a707d39dbe4688b
Nahuel <nahuel.lorenzodev@gmail.com>
2023-07-01 16:05:14 -0300

    fix validation of form

--- public/handleForm.js
+++ public/handleForm.js
@@ -18,129 +18,109 @@ function validateAddTeam(formData) {
   const stadium = formData.get('stadium');
   const shieldImg = formData.get('shieldImg');
 
-  let error = '';
-
   if (name === '') {
     const textError = 'Please, type the name';
     document.querySelector('.name__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.name__error').textContent = '';
-    error = true;
   }
 
   if (shortName === '') {
     const textError = 'Please, type the shortname';
     document.querySelector('.shortname__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.shortname__error').textContent = '';
-    error = true;
   }
 
   if (abbreviation === '') {
     const textError = 'Please, type the abbreviation';
     document.querySelector('.abbreviation__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.abbreviation__error').textContent = '';
-    error = true;
   }
 
   if (nameArea === '') {
     const textError = 'Please, type the name area';
     document.querySelector('.area-name__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.area-name__error').textContent = '';
-    error = true;
   }
 
   if (idArea === '') {
     const textError = 'Please, type the id area';
     document.querySelector('.area-id__error').textContent = textError;
-    error = false;
   } else if (isNaN(idArea)) {
     textError = 'The area id should be a number';
-    error = true;
   } else {
     document.querySelector('.area-id__error').textContent = '';
-    error = true;
   }
 
   if (address === '') {
     const textError = 'Please, type the address';
     document.querySelector('.address__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.address__error').textContent = '';
-    error = true;
   }
 
   if (phone === '') {
     const textError = 'Please, type the phone number';
     document.querySelector('.phone__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.phone__error').textContent = '';
-    error = true;
   }
 
   if (web === '') {
     const textError = 'Please, type the website';
     document.querySelector('.web__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.web__error').textContent = '';
-    error = true;
   }
 
   if (email === '') {
     const textError = 'Please, type the email';
     document.querySelector('.email__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.email__error').textContent = '';
-    error = true;
   }
 
   if (founded === '') {
     const textError = 'Please, type the date of its foundation';
     document.querySelector('.founded__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.founded__error').textContent = '';
-    error = true;
   }
 
   if (colors === '') {
     const textError = 'Please, type the colors';
     document.querySelector('.colors__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.colors__error').textContent = '';
-    error = true;
   }
 
   if (stadium === '') {
     const textError = 'Please, type the stadium';
     document.querySelector('.stadium__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.stadium__error').textContent = '';
-    error = true;
   }
 
   if (shieldImg.size === 0) {
     const textError = 'Please, insert a shield file';
     document.querySelector('.shield__error').textContent = textError;
-    error = false;
   } else {
     document.querySelector('.shield__error').textContent = '';
-    error = true;
   }
 
-  return error;
+  let counterErrors = 0;
+  document.querySelectorAll('.error').forEach(($error) => {
+    if ($error.textContent) {
+      counterErrors += 1;
+    }
+  });
+
+  return {
+    errors: counterErrors,
+  };
 }
 
 function handleSubmitForm() {
@@ -151,7 +131,8 @@ function handleSubmitForm() {
     const buttonText = event.target.querySelector('button').textContent;
     const formData = new FormData(event.target);
 
-    const isFormValid = validateAddTeam(formData);
+    const { errors } = validateAddTeam(formData);
+    const isFormValid = errors === 0;
 
     if (isFormValid) {
       if (buttonText === 'Send team') {
